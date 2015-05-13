602964e1ef0121f84655e91146fabcad6147a03f
Humberto Peña <humbertojpt@outlook.com>
2015-05-13 00:15:13 +0000

    Date validated and Tasks listed 2 cols

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,12 @@
 class Task < ActiveRecord::Base
 	 validates :title, :description,:duration, presence: true
 	 belongs_to :user
+
+	 validate :dates
+
+	 def dates
+	 	if !finish_date.blank? and finish_date < start_date
+	 		errors.add(:dates, "Wrong dates. Try again")
+	    end
+	end
 end
